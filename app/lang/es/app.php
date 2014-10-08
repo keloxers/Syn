95aad5d2bc5c1095d84b9338de6e9a0a16b09ad1
Miguel Mendez <keloxers@gmail.com>
2014-10-08 09:47:19 -0300

    upload 1

---
+++ app/lang/es/app.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Repositiory Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	'site' 		=>  "RenovacionSocial.com",
+
+	'title_site'		=>	"Renovacion Social",
+
+	'title_home' => "Renovacion Social<br>Corrientes Argentina",
+
+	'email'		=>	"<strong>info@renovacionsocial.com</strong>",
+
+	'telefono'		=>	"<strong>+54 3756 459030</strong>",
+
+	'seguinos_facebook'		=>	"",
+
+	'seguinos_twitter'		=>	"PartidoRSocial",
+
+	'seguinos_googleplus'		=>	"https://plus.google.com/u/0/117914111014830448957",
+
+	'seguinos_instagram'		=>	"",
+
+	'seguinos_youtube'		=>	"",
+
+	'direccion'		=>	"Virasoro, Corrientes AR",
+
+	'contacto'		=>	"(03756) 459030 <br><a href='mailto:info@renovacionsocial.com'>info@renovacionsocial.com</a>",
+
+	'title_food' => "Renovacion Social",
+
+''		=>	"",
+''		=>	"",
+''		=>	"",
+''		=>	"",
+''		=>	"",
+''		=>	"",
+
+
+
+
+);
